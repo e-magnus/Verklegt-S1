3c28d752329b39939e6c3d2fc885dceb2d22f45d
e-magnus <einare15@ru.is>
2015-11-30 11:26:10 +0000

    Breytinar á main fallinu - valmynd
    ######### THE COMPUTER SCIENTISTS DATABASE #########
    
    Please press 1 - 3 to select or Q to quit:
    
    1: See list of computer scientists in the database.
    2: Search database for computer scientist.
    3: Add new computer scientists to database.
    
    Selection:

--- Skil1/main.cpp
+++ Skil1/main.cpp
@@ -8,7 +8,19 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    cout << "Hello world!";
+    int selection;
+
+
+    cout << "######### THE COMPUTER SCIENTISTS DATABASE #########" << endl;
+    cout << endl;
+    cout << "Please press 1 - 3 to select or Q to quit: " << endl;
+    cout << endl;
+    cout << "1: See list of computer scientists in the database." << endl;
+    cout << "2: Search database for computer scientist. " << endl;
+    cout << "3: Add new computer scientists to database. " << endl;
+    cout << endl;
+    cout << "Selection: ";
+    cin >> selection;
 
     return a.exec();
 }
