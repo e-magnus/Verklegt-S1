a92756ca7f47be7ec69bbe7aabf9c545a8d8aa00
e-magnus <einare15@ru.is>
2015-11-30 16:12:42 +0000

    Nýr constructor fyrir compterscientist klasann

--- Skil1/computerscientist.cpp
+++ Skil1/computerscientist.cpp
@@ -5,10 +5,24 @@ ComputerScientist::ComputerScientist()
     id = 0;
     name = ' ';
     sex = ' ';
+    bio = ' ';
     born_year = 0;
     born_month = 0;
     born_day = 0;
     dead_year = 0;
     dead_month = 0;
     dead_day = 0;
+}
+
+ComputerScientist::ComputerScientist(string na, string se,int bd, int bm, int by, int dd, int dm, int dy, string bio)
+{
+    name = na;
+    sex = se;
+    bio = bio;
+    born_year = by;
+    born_month = bm;
+    born_day = bd;
+    dead_year = dy;
+    dead_month = dm;
+    dead_day = dd;
 }
--- Skil1/computerscientist.h
+++ Skil1/computerscientist.h
@@ -10,10 +10,12 @@ class ComputerScientist{
 
 public:
     ComputerScientist();
+    ComputerScientist(string na, string se,int bd, int bm, int by, int dd, int dm, int dy, string bio);
 private:
     int id;
     string name;
     string sex;
+    string bio;
     int born_year;
     int born_month;
     int born_day;
