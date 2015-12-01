6a9180d21612a41fdb5ec60c6e767be22a51e12a
e-magnus <einare15@ru.is>
2015-12-01 21:41:54 +0000

    Uppffærsla á void addScientist()
    Enn ekki farið að virka sem skyldi!
    Langar bara að fara að sofa...

--- Skil1/main.cpp
+++ Skil1/main.cpp
@@ -117,14 +117,14 @@ void addScientist()
 {
     ComputerScientist temp;
     string name;
-    char sex;
-    char info;
-    int born_year;
-    int born_month;
-    int born_day;
-    int dead_year;
-    int dead_month;
-    int dead_day;
+    char sex = ' ', alive;
+    int born_year = 0;
+    int born_month = 0;
+    int born_day = 0;
+    int dead_year = 0;
+    int dead_month = 0;
+    int dead_day = 0;
+    bool sexFlag = true, bornFlag = true, deathFlag = true;
 
     ofstream outfile;
 
@@ -138,26 +138,81 @@ void addScientist()
     cout << "Enter the name of the new scientist. " << endl;
     cout << "Name: ";
     cin >> name;
+    // Fæ ekki getline til að virka í Qt!
 
     cout << "Press 'M' for male and 'F' for female" << endl;
     cout << "Gender: ";
     cin >> sex;
 
-    cout << "Enter birthday on the format DD MM YYYY: " << endl;
-    cin >> born_day >> born_month >> born_year;
+    while (sexFlag)
+    {
+        switch (sex)
+        {
+        case 'M':
+            sexFlag = false;
+            break;
+        case 'm':
+            sexFlag = false;
+            sex = 'M';
+            break;
+        case 'F':
+            sexFlag = false;
+            break;
+        case 'f':
+            sexFlag = false;
+            sex = 'F';
+            break;
+        default: cout << "Invalid choice, try again!" << endl;
+            cout << "Gender: ";
+            cin >> sex;
+            break;
+        }
+    }
 
-    //cout << "Is the person still allive? (y/n) ";
+    while (bornFlag);
+    {
+        cout << "Enter birthday on the format DD MM YYYY: " << endl;
+        cin >> born_day >> born_month >> born_year;
+        if (born_day > 0 && born_day < 32 && born_month > 1 && born_month < 13 && born_year > 1850 && born_year < 2015)
+            bornFlag = false;
+        else
+            cout << "Invalid date! " << endl << endl;
+    }
 
-    cout << "Enter death day on the format DD MM YYYY: " << endl;
-    cin >> dead_day >> dead_month >> dead_year;
+    cout << "Is the person still allive? (y/n) ";
+    cin >> alive;
 
-    cout << "Pleas write short summary about the person. " << endl;
-    cin >> info;
+    while(deathFlag){
+        switch (alive) {
+        case 'y':
+            while (deathFlag);
+            {
+                cout << "Enter death day on the format DD MM YYYY: " << endl;
+                cin >> dead_day >> dead_month >> dead_year;
+                if (dead_day > 0 && dead_day < 32 && dead_month > 1 && dead_month < 13 && dead_year > 1850 && dead_year < 2015)
+                    deathFlag = false;
+                else
+                    cout << "Invalid date! " << endl << endl;
+            }
+            break;
+        case 'n':
+            deathFlag = false;
+            break;
+        default: cout << "Invalid choice, try again!" << endl;
+            cout << "Is the person still allive? (y/n) ";
+            cin >> alive;
+            break;
+        }
+    }
+
+
 
     cout << "Name: " << name << " (" << sex << ")" << endl;
     cout << "Born: " << born_day << "." << born_month << "." << born_year << endl;
     cout << "Died: " << dead_day << "." << dead_month << "." << dead_year << endl;
-    cout << info << endl;
+
+    outfile << name << ", " << sex << ", " << born_day << ", " << born_month << ", " << born_year << ", "
+            << dead_day << ", " << dead_month  << ", " << dead_year << ", ";
 
     outfile.close();
 
