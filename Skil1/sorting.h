64bdf5983682ecac5b5b29b1925954eb144b0231
arnarb94 <arnarb94@gmail.com>
2015-12-02 09:35:55 +0000

    Update sorting.h

--- Skil1/sorting.h
+++ Skil1/sorting.h
@@ -3,10 +3,12 @@
 #include <iostream>
 using namespace std;
 
-class Sorting
+class Sorting : ComputerScientist
 {
 public:
-    Sorting();
+    bool sortByName(Sorting a, Sorting b);
+    bool sortBySex(Sorting a, Sorting b);
+    bool sortByAge(Sorting a, Sorting b);
 };
 
 #endif // SORTING_H
